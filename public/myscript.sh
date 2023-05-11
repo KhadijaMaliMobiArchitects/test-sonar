0c16ce146a300d823d0a4b395e1eaea7edc92946
KhadijaMaliMobiArchitects <128594978+KhadijaMaliMobiArchitects@users.noreply.github.com>
2023-05-11 17:08:33 +0100

    Update myscript.sh

--- public/myscript.sh
+++ public/myscript.sh
@@ -1,7 +1,7 @@
 sudo su <<HERE
 cd /var/www/
 rm -rf  drop   
-cp -r $(System.DefaultWorkingDirectory)/_khadija-react-CI/drop /var/www
+cp -r $(System.DefaultWorkingDirectory)/_test-sonar-CI/drop /var/www
 chown -R www-data:www-data /var/www/drop
 chmod -R 755 /var/www/drop/
 HERE
