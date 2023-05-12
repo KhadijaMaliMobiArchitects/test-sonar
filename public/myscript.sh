8cc7b3badfe1e0b8d07847b75d36f64b50fe0d1b
KhadijaMaliMobiArchitects <128594978+KhadijaMaliMobiArchitects@users.noreply.github.com>
2023-05-12 16:58:38 +0100

    Update myscript.sh

--- public/myscript.sh
+++ public/myscript.sh
@@ -1,8 +1,6 @@
 sudo su <<HERE
 cd /var/www/
-mkdir drop 
-mv drop  $RANDOM-drop 
-cp -r $(System.DefaultWorkingDirectory)/_test-sonar-CI/drop /var/www/drop
+cp -rf $(System.DefaultWorkingDirectory)/_test-sonar-CI/drop /var/www/drop
 chown -R www-data:www-data /var/www/drop
 chmod -R 755 /var/www/drop/
 HERE
