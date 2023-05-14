f77a66e5f66e1b7672bbf0df1dabab32a5da2035
KhadijaMaliMobiArchitects <128594978+KhadijaMaliMobiArchitects@users.noreply.github.com>
2023-05-14 21:51:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload vmssssssss.
+          Edit <code>src/App.js</code> and save to reload see vmss :))
         </p>
         <a
           className="App-link"
