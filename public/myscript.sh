106fe4c2b77f2c5d5594c19ade72360104d7b060
KhadijaMaliMobiArchitects <128594978+KhadijaMaliMobiArchitects@users.noreply.github.com>
2023-05-14 20:49:14 +0100

    Update myscript.sh

--- public/myscript.sh
+++ public/myscript.sh
@@ -1,12 +1,6 @@
 sudo su <<HERE
-cd /var/www/
-tessstttcopy
+cd /var/www/   
+cp -rf /home/khadija/drop /var/www
 chown -R www-data:www-data /var/www/drop
-test22 pour savoir si ça se remplace
-test3 
-apres suppression de drop manuellement
-clear target folder
-apres ajout de instance 4 qui la configuration de vm image et ne ressemble pas a l instance existante
-apres release je trouve encore la derniere instance vide, essaie2
 chmod -R 755 /var/www/drop/
 HERE
