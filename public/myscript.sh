c9c8fef40545ad712128d5ef2f6f166349cb57f6
KhadijaMaliMobiArchitects <128594978+KhadijaMaliMobiArchitects@users.noreply.github.com>
2023-05-18 00:10:08 +0100

    Update myscript.sh

--- public/myscript.sh
+++ public/myscript.sh
@@ -1,3 +1,3 @@
 sudo su <<HERE
-azcopy copy 'https://projecttstorageaccount.blob.core.windows.net/vet/drop/??sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-12-30T06:11:42Z&st=2023-05-17T22:11:42Z&spr=https&sig=eEXl6tGp0lizMJWKnudq9Pvhkv55cgrj8yCq5k759OQ%3D' '/var/www/' --recursive=true
+azcopy copy 'https://projecttstorageaccount.blob.core.windows.net/vet/drop/?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-12-30T06:11:42Z&st=2023-05-17T22:11:42Z&spr=https&sig=eEXl6tGp0lizMJWKnudq9Pvhkv55cgrj8yCq5k759OQ%3D' '/var/www/' --recursive=true
 HERE
